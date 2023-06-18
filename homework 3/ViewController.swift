43e88eec40c106a48b8101b100c4f57628a2fcb0
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-18 22:41:09 +0600

    Initial Commit

---
+++ homework 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework 3
+//
+//  Created by Ilya Pogozhev on 18.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
